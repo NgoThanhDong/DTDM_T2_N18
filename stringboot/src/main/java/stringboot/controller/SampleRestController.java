f93ea3a8f49a7264e4fa6c14a573d3e7ea36f452
Ngo Thanh Dong <dongnt2015@gmail.com>
2017-03-09 10:42:58 +0700

    Spring Boot project đơn giản
    Spring Boot project đơn giản

---
+++ stringboot/src/main/java/stringboot/controller/SampleRestController.java
@@ -0,0 +1,13 @@
+package stringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleRestController {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello World!!!";
+	}
+}
